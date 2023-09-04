ea847b2c5f7c6d0fd970071f228fd3cf9254e58d
ruslan-byte <rbaroev0@gmail.com>
2023-09-04 11:30:49 +0300

    correc t initansection

--- components/InstantSection.vue
+++ components/InstantSection.vue
@@ -41,37 +41,8 @@
 <script setup>
 	import { showModal } from '~/assets/js/components/modal.js';
 	import {useStore} from 'vuex';
+	import { staticData } from "~/assets/json/intantStikerStatic";
 	const store = useStore();
-	const staticData = [
-		{
-			srcset: "images/instant-card-img1@1x.webp, images/instant-card-img1@2x.webp 2x",
-			imgSrcset: "images/instant-card-img1@2x.png 2x"
-		},
-		{
-			srcset: "images/instant-card-img2@1x.webp, images/instant-card-img2@2x.webp 2x",
-			imgSrcset: "images/instant-card-img2@2x.png 2x"
-		},
-		{
-			srcset: "images/instant-card-img3@1x.webp, images/instant-card-img3@2x.webp 2x",
-			imgSrcset: "images/instant-card-img3@2x.png 2x"
-		},
-		{
-			srcset: "images/instant-card-img4@1x.webp, images/instant-card-img4@2x.webp 2x",
-			imgSrcset: "images/instant-card-img4@2x.png 2x"
-		},
-		{
-			srcset: "images/instant-card-img5@1x.webp, images/instant-card-img5@2x.webp 2x",
-			imgSrcset: "images/instant-card-img5@2x.png 2x"
-		},
-		{
-			srcset: "images/instant-card-img6@1x.webp, images/instant-card-img6@2x.webp 2x",
-			imgSrcset: "images/instant-card-img6@2x.png 2x"
-		},
-		{
-			srcset: "images/instant-card-img7@1x.webp, images/instant-card-img7@2x.webp 2x",
-			imgSrcset: "images/instant-card-img7@2x.png 2x"
-		},
-	]
 	function openStickerDetail(stickerData)
 	{
 		if(stickerData.activatedCount){
